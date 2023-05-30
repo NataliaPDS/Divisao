85fd7853d05e98b3f7363fda5af970f6cdf7671c
Natalia <natpereira9102@gmail.com>
2023-05-30 15:28:25 -0400

    Segundo commit

--- TesteGit/Program.cs
+++ TesteGit/Program.cs
@@ -1,7 +1,16 @@
-﻿Console.WriteLine("DIVISÃO");
-Console.WriteLine("Digite o primeiro número: ");
-int a = int.Parse(Console.ReadLine());
-Console.WriteLine("Digite o segundo número: ");
-int b = int.Parse(Console.ReadLine());
-int result = a / b;
-Console.WriteLine($"\nResultado = {result}");
+﻿try
+{
+    Console.WriteLine("DIVISÃO");
+
+    Console.WriteLine("Digite o primeiro número: ");
+    int a = int.Parse(Console.ReadLine());
+    Console.WriteLine("Digite o segundo número: ");
+    int b = int.Parse(Console.ReadLine());
+
+    int result = a / b;
+    Console.WriteLine($"\nResultado = {result}");
+
+}catch(Exception ex)//10 / 0 = está mensagem
+{
+    Console.WriteLine("\nAlgo de errado não está certo!");
+}

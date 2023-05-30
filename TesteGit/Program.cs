ea9c1a9414b68b6aa278639a17f4c07ffafb65d8
Natalia <natpereira9102@gmail.com>
2023-05-30 15:02:51 -0400

    Inicio do projeto de divisao de dois numeros

---
+++ TesteGit/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine("DIVISÃO");
+Console.WriteLine("Digite o primeiro número: ");
+int a = int.Parse(Console.ReadLine());
+Console.WriteLine("Digite o segundo número: ");
+int b = int.Parse(Console.ReadLine());
+int result = a / b;
+Console.WriteLine($"\nResultado = {result}");
